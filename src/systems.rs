9ab4492b213f87010e7660e94dd8921d06956d99
mvlabat <mvlabat@gmail.com>
2021-01-16 21:20:09 +0200

    Add support for all the egui key codes

--- src/systems.rs
+++ src/systems.rs
@@ -58,7 +58,11 @@ pub fn process_input(
     let alt = keyboard_input.pressed(KeyCode::LAlt) || keyboard_input.pressed(KeyCode::RAlt);
     let win = keyboard_input.pressed(KeyCode::LWin) || keyboard_input.pressed(KeyCode::RWin);
 
-    let mac_cmd = if cfg!(target_os = "macos") { win } else { false };
+    let mac_cmd = if cfg!(target_os = "macos") {
+        win
+    } else {
+        false
+    };
     let command = if cfg!(target_os = "macos") { win } else { ctrl };
 
     let modifiers = egui::Modifiers {
@@ -69,7 +73,7 @@ pub fn process_input(
         command,
     };
 
-    if !command {
+    if !ctrl && !win {
         for event in egui_context.received_character.iter(&ev_received_character) {
             if event.id.is_primary() && !event.char.is_control() {
                 egui_context
@@ -80,118 +84,25 @@ pub fn process_input(
         }
     }
 
-    if keyboard_input.just_pressed(KeyCode::Up) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::ArrowUp,
-            pressed: true,
-            modifiers,
-        });
+    for pressed_key in keyboard_input.get_just_pressed() {
+        if let Some(key) = bevy_to_egui_key(*pressed_key) {
+            egui_context.raw_input.events.push(egui::Event::Key {
+                key,
+                pressed: true,
+                modifiers,
+            })
+        }
     }
-    if keyboard_input.just_pressed(KeyCode::Down) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::ArrowDown,
-            pressed: true,
-            modifiers,
-        });
+    for pressed_key in keyboard_input.get_just_released() {
+        if let Some(key) = bevy_to_egui_key(*pressed_key) {
+            egui_context.raw_input.events.push(egui::Event::Key {
+                key,
+                pressed: false,
+                modifiers,
+            })
+        }
     }
-    if keyboard_input.just_pressed(KeyCode::Right) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::ArrowRight,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Left) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::ArrowLeft,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Home) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::Home,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::End) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::End,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Delete) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::Delete,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Back) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::Backspace,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Return) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::Enter,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Tab) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::Tab,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::A) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::A,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::K) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::K,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::K) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::K,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::U) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::U,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::W) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::W,
-            pressed: true,
-            modifiers,
-        });
-    }
-    if keyboard_input.just_pressed(KeyCode::Z) {
-        egui_context.raw_input.events.push(egui::Event::Key {
-            key: egui::Key::Z,
-            pressed: true,
-            modifiers,
-        });
-    }
+
     egui_context.raw_input.predicted_dt = time.delta_seconds();
     egui_context.raw_input.modifiers = modifiers;
 }
@@ -199,4 +110,62 @@ pub fn process_input(
 pub fn begin_frame(mut egui_context: ResMut<EguiContext>) {
     let raw_input = egui_context.raw_input.take();
     egui_context.ctx.begin_frame(raw_input);
+}
+
+fn bevy_to_egui_key(key_code: KeyCode) -> Option<egui::Key> {
+    let key = match key_code {
+        KeyCode::Down => egui::Key::ArrowDown,
+        KeyCode::Left => egui::Key::ArrowLeft,
+        KeyCode::Right => egui::Key::ArrowRight,
+        KeyCode::Up => egui::Key::ArrowUp,
+        KeyCode::Escape => egui::Key::Escape,
+        KeyCode::Tab => egui::Key::Tab,
+        KeyCode::Back => egui::Key::Backspace,
+        KeyCode::Return => egui::Key::Enter,
+        KeyCode::Space => egui::Key::Space,
+        KeyCode::Insert => egui::Key::Insert,
+        KeyCode::Delete => egui::Key::Delete,
+        KeyCode::Home => egui::Key::Home,
+        KeyCode::End => egui::Key::End,
+        KeyCode::PageUp => egui::Key::PageUp,
+        KeyCode::PageDown => egui::Key::PageDown,
+        KeyCode::Numpad0 | KeyCode::Key0 => egui::Key::Num0,
+        KeyCode::Numpad1 | KeyCode::Key1 => egui::Key::Num1,
+        KeyCode::Numpad2 | KeyCode::Key2 => egui::Key::Num2,
+        KeyCode::Numpad3 | KeyCode::Key3 => egui::Key::Num3,
+        KeyCode::Numpad4 | KeyCode::Key4 => egui::Key::Num4,
+        KeyCode::Numpad5 | KeyCode::Key5 => egui::Key::Num5,
+        KeyCode::Numpad6 | KeyCode::Key6 => egui::Key::Num6,
+        KeyCode::Numpad7 | KeyCode::Key7 => egui::Key::Num7,
+        KeyCode::Numpad8 | KeyCode::Key8 => egui::Key::Num8,
+        KeyCode::Numpad9 | KeyCode::Key9 => egui::Key::Num9,
+        KeyCode::A => egui::Key::A,
+        KeyCode::B => egui::Key::B,
+        KeyCode::C => egui::Key::C,
+        KeyCode::D => egui::Key::D,
+        KeyCode::E => egui::Key::E,
+        KeyCode::F => egui::Key::F,
+        KeyCode::G => egui::Key::G,
+        KeyCode::H => egui::Key::H,
+        KeyCode::I => egui::Key::I,
+        KeyCode::J => egui::Key::J,
+        KeyCode::K => egui::Key::K,
+        KeyCode::L => egui::Key::L,
+        KeyCode::M => egui::Key::M,
+        KeyCode::N => egui::Key::N,
+        KeyCode::O => egui::Key::O,
+        KeyCode::P => egui::Key::P,
+        KeyCode::Q => egui::Key::Q,
+        KeyCode::R => egui::Key::R,
+        KeyCode::S => egui::Key::S,
+        KeyCode::T => egui::Key::T,
+        KeyCode::U => egui::Key::U,
+        KeyCode::V => egui::Key::V,
+        KeyCode::W => egui::Key::W,
+        KeyCode::X => egui::Key::X,
+        KeyCode::Y => egui::Key::Y,
+        KeyCode::Z => egui::Key::Z,
+        _ => return None,
+    };
+    Some(key)
 }
