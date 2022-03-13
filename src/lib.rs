4f4b0208740844cb22f1bc3bf4ae6f9e5872f6fe
mvlabat <mvlabat@gmail.com>
2022-03-13 11:47:47 +0200

    Remove the mention of bevy_webgl2 from the docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! An example WASM project is live at [mvlabat.github.io/bevy_egui_web_showcase](https://mvlabat.github.io/bevy_egui_web_showcase/index.html) [[source](https://github.com/mvlabat/bevy_egui_web_showcase)].
 //!
 //! **Features:**
-//! - Desktop and web ([bevy_webgl2](https://github.com/mrk-its/bevy_webgl2)) platforms support
+//! - Desktop and web platforms support
 //! - Clipboard (web support is limited to the same window, see [rust-windowing/winit#1829](https://github.com/rust-windowing/winit/issues/1829))
 //! - Opening URLs
 //! - Multiple windows support (see [./examples/two_windows.rs](./examples/two_windows.rs))
