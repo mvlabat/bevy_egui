94e054e9840ace839b85c3d52b867b09db5183f7
oscrim <oscar@widefind.se>
2022-09-06 14:12:16 +0200

    Fixed touch bug

--- src/lib.rs
+++ src/lib.rs
@@ -533,6 +533,7 @@ impl Plugin for EguiPlugin {
         world.init_resource::<EguiClipboard>();
         world.init_resource::<EguiUserTextures>();
         world.init_resource::<EguiMousePosition>();
+        world.insert_resource(TouchId::default());
 
         app.add_startup_systems(
             (
